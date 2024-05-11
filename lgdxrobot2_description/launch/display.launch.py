4ce3c268c0ab4861b5a3f645868f307eb68e21ee
Kai Tung Yu <sam@bristolgram.uk>
2024-05-11 11:48:50 +0100

    webots show rviz

--- lgdxrobot2_description/launch/display.launch.py
+++ lgdxrobot2_description/launch/display.launch.py
@@ -8,7 +8,9 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
   package_dir = get_package_share_directory('lgdxrobot2_description')
   model = LaunchConfiguration('model')
-  rviz_config = LaunchConfiguration('rvizconfig')
+  rviz_config = LaunchConfiguration('rviz_config')
+  use_sim_time = LaunchConfiguration('use_sim_time')
+  use_rviz = LaunchConfiguration('use_rviz')
 
   robot_state_publisher_node = Node(
     package='robot_state_publisher',
@@ -27,7 +29,9 @@ def generate_launch_description():
     executable='rviz2',
     name='rviz2',
     output='screen',
+    parameters=[{'use_sim_time': use_sim_time}],
     arguments=['-d', PathJoinSubstitution([package_dir, 'rviz', rviz_config])],
+    condition=launch.conditions.IfCondition(use_rviz)
   )
 
   return launch.LaunchDescription([
@@ -37,9 +41,19 @@ def generate_launch_description():
       description='Model file in lgdxrobot2_description package.'
     ),
     DeclareLaunchArgument(
-      name='rvizconfig', 
+      name='rviz_config', 
       default_value='urdf_config.rviz',
       description='Rviz config file in lgdxrobot2_description package.'
+    ),
+    DeclareLaunchArgument(
+      name='use_sim_time',
+      default_value='True',
+      description='Use the simulation time from Webots.'
+    ),
+    DeclareLaunchArgument(
+      name='use_rviz',
+      default_value='True',
+      description='Launch RVIZ2.'
     ),
     joint_state_publisher_node,
     robot_state_publisher_node,
--- lgdxrobot2_webots/launch/robot.launch.py
+++ lgdxrobot2_webots/launch/robot.launch.py
@@ -1,18 +1,22 @@
 import launch
 from launch.substitutions import LaunchConfiguration
 from launch.substitutions.path_join_substitution import PathJoinSubstitution
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_controller import WebotsController
+from webots_ros2_driver.wait_for_controller_connection import WaitForControllerConnection
 import os
 
 def generate_launch_description():
   package_dir = get_package_share_directory('lgdxrobot2_webots')
+  description_package_dir = get_package_share_directory('lgdxrobot2_description')
   world = LaunchConfiguration('world')
-  use_sim_time = LaunchConfiguration('use_sim_time')
   robot_description_path = os.path.join(package_dir, 'resource', 'lgdxrobot2.urdf')
+  use_sim_time = LaunchConfiguration('use_sim_time')
+  use_rviz = LaunchConfiguration('use_rviz')
   
   webots = WebotsLauncher(
     world=PathJoinSubstitution([package_dir, 'worlds', world]),
@@ -30,21 +34,43 @@ def generate_launch_description():
     respawn=True
   )
   
+  description_nodes = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(
+      os.path.join(description_package_dir, 'launch', 'display.launch.py')
+    ),
+    launch_arguments={
+      'model': 'lgdxrobot2_simulation.urdf',
+      'use_rviz': use_rviz,
+      'use_sim_time': use_sim_time,
+    }.items(),
+  )
+  
+  waiting_nodes = WaitForControllerConnection(
+    target_driver=lgdxrobot2_driver,
+    nodes_to_start=[description_nodes]
+  )
+  
   return LaunchDescription([
     DeclareLaunchArgument(
       'world',
       default_value='world.wbt',
-      description='Choose one of the world files'
+      description='Choose one of the world files.'
     ),
     DeclareLaunchArgument(
       name='use_sim_time',
       default_value='True',
       description='Use the simulation time from Webots.'
     ),
+    DeclareLaunchArgument(
+      name='use_rviz',
+      default_value='True',
+      description='Launch RVIZ2.'
+    ),
     webots,
     webots._supervisor,
 
     lgdxrobot2_driver,
+    waiting_nodes,
 
     # This action will kill all nodes once the Webots simulation has exited
     launch.actions.RegisterEventHandler(
