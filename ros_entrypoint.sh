062d17bb00b61a1cb59da38d1b68f112eeaea77b
Kai Tung Yu <sam@bristolgram.uk>
2025-07-17 14:48:45 +0100

    revert

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/home/user/webots_ws/install/setup.bash"
-source "/home/user/lgdx_ws/install/setup.bash" --
+source "/home/user/lgdx_ws/install/setup.bash"
 exec "$@"
