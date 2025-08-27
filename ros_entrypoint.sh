c0a4109efd4aafb5ee1a79b86763afc9cf16a987
Kai Tung Yu <sam@bristolgram.uk>
2025-08-27 15:23:52 +0100

    update docker

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
+# Setup permissions
+chown -R abc:abc /config/.ros
+chown -R abc:abc /config/.colcon
+chown -R abc:abc /config/lgdx_ws
+chown -R abc:abc /config/webots_ws
+
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/home/user/webots_ws/install/setup.bash"
-source "/home/user/lgdx_ws/install/setup.bash"
+source "/config/webots_ws/install/setup.bash"
+source "/config/lgdx_ws/install/setup.bash"
 exec "$@"
