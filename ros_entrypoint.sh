9a3727316181556c56a062a91ca0cf62fe4a516e
Kai Tung Yu <sam@bristolgram.uk>
2025-10-15 13:53:27 +0100

    fix folder  bug

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -2,11 +2,16 @@
 set -e
 
 # Setup permissions
+mkdir -p /config/.ros
+mkdir -p /config/.colcon
+mkdir -p /config/lgdx_ws
+mkdir -p /config/webots_ws
+mkdir -p /config/webots
+
 chown -R 1000:1000 /config/.ros
 chown -R 1000:1000 /config/.colcon
 chown -R 1000:1000 /config/lgdx_ws
 chown -R 1000:1000 /config/webots_ws
-mkdir -p /config/webots
 chown -R 1000:1000 /config/webots
 
 # setup ros2 environment
