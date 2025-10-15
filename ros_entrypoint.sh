d38dc9d796a4382c3cc3736d4edc48bd5c765307
Kai Tung Yu <sam@bristolgram.uk>
2025-10-15 14:57:59 +0100

    update

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -2,11 +2,7 @@
 set -e
 
 # Setup permissions
-chown -R 1000:1000 /config/.ros
-chown -R 1000:1000 /config/.colcon
-chown -R 1000:1000 /config/lgdx_ws
-chown -R 1000:1000 /config/webots_ws
-chown -R 1000:1000 /config/webots
+chown -R 1000:1000 /config
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
