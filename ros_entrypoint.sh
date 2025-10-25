59ba2c15fa3ec219c0a18b72a810b218b56a0eb3
Kai Tung Yu <sam@bristolgram.uk>
2025-10-25 13:24:16 +0100

    not chmod all

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -2,10 +2,31 @@
 set -e
 
 # Setup permissions
-chown -R 1000:1000 /config
+if [[ -e /config/.ros ]]; then
+  chown -R 1000:1000 /config/.ros
+fi
+
+if [[ -e /config/.colcon ]]; then
+  chown -R 1000:1000 /config/.colcon
+fi
+
+if [[ -e /config/lgdx_ws ]]; then
+  chown -R 1000:1000 /config/lgdx_ws
+fi
+
+if [[ -e /config/webots_ws ]]; then
+  chown -R 1000:1000 /config/webots_ws
+fi
+
+if [[ -e /config/webots ]]; then
+  chown -R 1000:1000 /config/webots
+fi
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/config/webots_ws/install/setup.bash"
 source "/config/lgdx_ws/install/setup.bash"
+
+echo "Setup complete"
+
 exec "$@"
